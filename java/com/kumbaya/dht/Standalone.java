3daf1fbbe4472f6e21b5dd4322852f35001d7acd
goto <samuelgoto@gmail.com>
2014-04-27 10:28:10 -0700

    .

--- java/com/kumbaya/dht/Standalone.java
+++ java/com/kumbaya/dht/Standalone.java
@@ -9,9 +9,13 @@ import com.kumbaya.dht.MessageDispatcherTest.MessageDispatcherFactoryImpl;
 
 public class Standalone {
 	public static void main(String[] args) throws Exception {
+		int port = System.getenv().containsKey("PORT") ? 
+				Integer.valueOf(System.getenv("PORT")) :
+				8080;
+		
 		Context dht = (Context) MojitoFactory.createDHT("bootstrap");
 		dht.setMessageDispatcher(new MessageDispatcherFactoryImpl());
-		dht.bind(new InetSocketAddress("localhost", 8080));
+		dht.bind(new InetSocketAddress("localhost", port));
 		dht.start();
 	}
 }
